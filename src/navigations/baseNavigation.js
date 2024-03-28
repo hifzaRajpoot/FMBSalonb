d7e8b62bf0dbc18466a60b62deb3a3066e883184
Your Name <hifzarajpootlearning@gmail.com>
2024-03-28 10:25:42 +0500

    before navigations

---
+++ src/navigations/baseNavigation.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function baseNavigation() {
+  return (
+    <View>
+      <Text>baseNavigation</Text>
+    </View>
+  )
+}
