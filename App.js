f1bea15c588229ef9c1e00d4d65eec5d5202c067
Your Name <hifzarajpootlearning@gmail.com>
2024-03-28 09:30:10 +0500

    tail wind installed

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import {Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className={"flex  flex-1 bg-orange-400 justify-center center-item"}>
+      <Text className={"text-2xl font-bold text-green-400"}>
+        Open up App.js to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
